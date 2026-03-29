20f2268fd9d59625de40ddc66c02b4d20ffb9f12
Roberto <rkyuchukov18@gmail.com>
2026-03-29 12:59:59 +0300

    Apel in functia main

--- Zatreanu_Roberto_ActivitateSD2026/Activitate_Roberto_Zatreanu.c
+++ Zatreanu_Roberto_ActivitateSD2026/Activitate_Roberto_Zatreanu.c
@@ -17,7 +17,6 @@ struct Telefon initializare(int id, int capacitate, const char* marca, float pre
 
     t.marca = (char*)malloc(strlen(marca) + 1);
     if (t.marca == NULL) {
-        
         t.marca = NULL;
     }
     else {
@@ -27,6 +26,10 @@ struct Telefon initializare(int id, int capacitate, const char* marca, float pre
     t.pret = pret;
     t.model = model;
     return t;
+}
+
+struct Telefon copiaza(struct Telefon t) {
+    return initializare(t.id, t.capacitateBaterie, t.marca, t.pret, t.model);
 }
 
 void afisare(struct Telefon t) {
@@ -50,54 +53,125 @@ void dezalocare(struct Telefon** vector, int* nrElemente) {
     *vector = NULL;
     *nrElemente = 0;
 }
+
 struct Telefon* copiazaPrimeleNElemente(struct Telefon* vector, int nrElemente, int nrElementeCopiate) {
-    if (nrElementeCopiate < nrElemente) {
-        		struct Telefon* vectorNou = malloc(sizeof(Telefon) * nrElementeCopiate);
-        		for (int i = 0; i < nrElementeCopiate; i++) {
-        			vectorNou[i] = copiaza(vector[i]);
-        		}
-        		return vectorNou;
-        	}
-        	else {
-        		return NULL;
-        	}
+    if (nrElementeCopiate <= nrElemente && nrElementeCopiate > 0) {
+        struct Telefon* vectorNou = (struct Telefon*)malloc(sizeof(struct Telefon) * nrElementeCopiate);
+        if (vectorNou == NULL) {
+            return NULL;
+        }
+
+        for (int i = 0; i < nrElementeCopiate; i++) {
+            vectorNou[i] = copiaza(vector[i]);
+        }
+        return vectorNou;
+    }
+    else {
+        return NULL;
+    }
 }
-void copiazaAnumiteElemente(struct Telefon* vector, char nrElemente, float prag, struct Telefon** vectorNou, int* dimensiune) {
-    	*dimensiune = 0;
-	for (int i = 0; i < nrElemente; i++) {
-		if (vector[i].suprafata < prag) {
-			(*dimensiune)++;
-		}
-	}
-	*vectorNou = malloc(sizeof(Telefon) * (*dimensiune));
-	*dimensiune = 0;
-	for (int i = 0; i < nrElemente; i++) {
-		if (vector[i].suprafata < prag) {
-			(*vectorNou)[*dimensiune] = copiaza(vector[i]);
-			(*dimensiune)++;
-		}
-	}
+
+void copiazaAnumiteElemente(struct Telefon* vector, int nrElemente, float prag, struct Telefon** vectorNou, int* dimensiune) {
+    *dimensiune = 0;
+
+    for (int i = 0; i < nrElemente; i++) {
+        if (vector[i].pret < prag) {
+            (*dimensiune)++;
+        }
+    }
+
+    *vectorNou = (struct Telefon*)malloc(sizeof(struct Telefon) * (*dimensiune));
+    if (*vectorNou == NULL && *dimensiune > 0) {
+        *dimensiune = 0;
+        return;
+    }
+
+    int k = 0;
+    for (int i = 0; i < nrElemente; i++) {
+        if (vector[i].pret < prag) {
+            (*vectorNou)[k] = copiaza(vector[i]);
+            k++;
+        }
+    }
 }
 
 struct Telefon getPrimulElementConditionat(struct Telefon* vector, int nrElemente, const char* conditie) {
-        Telefon s;
-    	s.id = -1;
-    	s.denumire = NULL;
-    	for (int i = 0; i < nrElemente; i++) {
-    		if (strcmp(numeCautat, vector[i].denumire)==0) {
-    			s = copiaza(vector[i]);
-    		}
-    	}
-    	return s;
+    struct Telefon s;
+    s.id = -1;
+    s.capacitateBaterie = 0;
+    s.marca = NULL;
+    s.pret = 0;
+    s.model = '-';
+
+    for (int i = 0; i < nrElemente; i++) {
+        if (strcmp(conditie, vector[i].marca) == 0) {
+            s = copiaza(vector[i]);
+            return s;
+        }
+    }
+
+    return s;
 }
+
 int main() {
-   
-    int n = 1;
+    int n = 4;
     struct Telefon* v = (struct Telefon*)malloc(sizeof(struct Telefon) * n);
+
+    if (v == NULL) {
+        return 1;
+    }
+
     v[0] = initializare(1, 2000, "Samsung", 200.00f, 'S');
+    v[1] = initializare(2, 3000, "Apple", 500.00f, 'A');
+    v[2] = initializare(3, 2500, "Samsung", 150.00f, 'M');
+    v[3] = initializare(4, 4000, "Xiaomi", 180.00f, 'X');
 
-    afisare(v[0]);
+    printf("Vector initial:\n");
+    for (int i = 0; i < n; i++) {
+        afisare(v[i]);
+    }
 
-    dezalocare(&v, &n);  
+    printf("\nPrimele 2 elemente copiate:\n");
+    struct Telefon* primele2 = copiazaPrimeleNElemente(v, n, 2);
+    if (primele2 != NULL) {
+        for (int i = 0; i < 2; i++) {
+            afisare(primele2[i]);
+        }
+    }
+
+    printf("\nTelefoane cu pret mai mic decat 250:\n");
+    struct Telefon* vectorFiltrat = NULL;
+    int dimFiltrat = 0;
+    copiazaAnumiteElemente(v, n, 250.0f, &vectorFiltrat, &dimFiltrat);
+    for (int i = 0; i < dimFiltrat; i++) {
+        afisare(vectorFiltrat[i]);
+    }
+
+    printf("\nPrimul telefon cu marca Samsung:\n");
+    struct Telefon t = getPrimulElementConditionat(v, n, "Samsung");
+    if (t.id != -1) {
+        afisare(t);
+        free(t.marca);
+    }
+    else {
+        printf("Nu s-a gasit telefonul cautat.\n");
+    }
+
+    if (primele2 != NULL) {
+        for (int i = 0; i < 2; i++) {
+            free(primele2[i].marca);
+        }
+        free(primele2);
+    }
+
+    if (vectorFiltrat != NULL) {
+        for (int i = 0; i < dimFiltrat; i++) {
+            free(vectorFiltrat[i].marca);
+        }
+        free(vectorFiltrat);
+    }
+
+    dezalocare(&v, &n);
+
     return 0;
 }
